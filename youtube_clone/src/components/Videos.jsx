a237c166bffd43d3258e6dc7f523eb68c004c807
Debangsu Das <debangsudas90@outlook.com>
2022-09-06 20:07:30 +0530

    3rd commit

--- youtube_clone/src/components/Videos.jsx
+++ youtube_clone/src/components/Videos.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Stack,Box } from '@mui/material'
+import ChannelCard from './ChannelCard'
+import VideoCard from './VideoCard'
 
 const Videos = ({ videos }) => {
-    console.log(videos)
+  console.log(videos)
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item,idx) => (
+        <Box key = {idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
