38f101aeb9027950cd9560490246fb1cef1b3e53
Debangsu Das <debangsudas90@outlook.com>
2022-10-24 00:17:12 +0530

    first commit

---
+++ portfolio/src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello There! <br/>
+        My name is Debangsu Das <br/>
+      </SectionTitle>
+      <SectionText>
+        I'm a front end web developer who enjoys building fun and rich interactive websites. Check out my works >>
+      </SectionText>
+      <Button onClick={() => window.location = "#tech"}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
