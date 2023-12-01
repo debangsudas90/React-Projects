c57404b60212a0e3fe81f9b840c530a4ef6db363
Debangsu Das <debangsudas90@outlook.com>
2023-12-01 18:39:08 +0530

    adding other functions

--- mern-stack/frontend/src/components/WorkoutDetails.js
+++ mern-stack/frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({workout}) => {
+
+  const handleClick = async() => {
+    const response = await fetch("http://localhost:4000/api/workouts/" + workout._id, {
+      method: 'DELETE',
+    })
+
+    const json = await response.json()
+
+    if(response.ok) {
+      console.log("deleted succesfully", json)
+    }
+  }
+
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
         <p><strong>Sets : </strong>{workout.sets}</p>
         <p><strong>Reps : </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
+        <p>{formatDistanceToNow(new Date(workout.createdAt), {addSuffix: true})}</p>
+        <span className="material-symbols-outlined" onClick={handleClick}>Delete</span>
     </div>
   )
 }
--- mern-stack/frontend/src/pages/Home.js
+++ mern-stack/frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 //components
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
 
@@ -11,7 +12,7 @@ const Home = () => {
     const fetchWorkouts = async() => {
       const response = await fetch("http://localhost:4000/api/workouts")
       const json = await response.json()
-      console.log(response)
+      // console.log(response)
 
       if(response.ok) {
         setWorkout(json)
@@ -20,7 +21,7 @@ const Home = () => {
 
     fetchWorkouts()
   
-  }, [])
+  }, [workouts])
   
 
   return (
@@ -30,6 +31,7 @@ const Home = () => {
           <WorkoutDetails key={workout._id} workout={workout}/>
         ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
